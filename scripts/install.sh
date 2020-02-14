34fe99246259a2fe785705b1fb8f75162fb15147
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2020-02-14 18:18:12 +0100

    Installation procedure update #2

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,11 @@
 sudo apt-get install python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential
 sudo rosdep init
 rosdep update
-cd ~/catkin_ws/
+mkdir ~/ros_install_ws/
+cd ~/ros_install_ws/
 rosinstall_generator perception --rosdistro melodic --deps --wet-only --tar > melodic-perception-wet.rosinstall
 wstool init src melodic-perception-wet.rosinstall
 rosdep install --from-paths src --ignore-src --rosdistro melodic -y
 ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
-source ~/catkin_ws/install_isolated/setup.bash
-echo 'source ~/catkin_ws/install_isolated/setup.bash' >> ~/.bashrc
+source ~/ros_install_ws/install_isolated/setup.bash
+echo 'source ~/ros_install_ws/install_isolated/setup.bash' >> ~/.bashrc
