5ca7d16bcf62809ec706fc9cccd17b0534b90032
Bjarne Johannsen <bjarne.johannsen@outlook.de>
2020-03-07 15:38:22 +0000

    Colored Images and removed multithreading

--- src/main.cpp
+++ src/main.cpp
@@ -36,16 +36,6 @@
 #include <sensor_msgs/Image.h>
 #include <chrono>
 
-#include <thread>
-#include <stdlib.h>
-#define MAX 16 
-#define MAX_THREAD 4 
-int num_threads = 4;
-
-int a[] = { 1, 5, 7, 10, 12, 14, 15, 18, 20, 22, 25, 27, 30, 64, 110, 220 }; 
-int sum[4] = { 0 }; 
-int part = 0; 
-
 //Define point cloud and images
 typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
 PointCloud::Ptr m_nimbus_cloud(new PointCloud);
@@ -57,29 +47,109 @@ AutoExposureParams_t m_params;
 //Global variables
 bool m_new_image = false;
 bool m_auto_exposure_update = false;
-#define m_img_width 352
-#define m_img_height 286
+const int m_img_width = 352;
+const int m_img_height = 286;
 const float m_XYZ_to_m = 0.0002;  //<-- to be determined. Correct value missings
 
-int16_t* test = 0;
 
 
-void call_from_thread(int tid) {    
-    int thread_part = part++;
 
-	for (int i = thread_part * (MAX / 4); i < (thread_part + 1) * (MAX / 4); i++) 
-		sum[thread_part] += a[i]; 
+typedef struct RgbColor
+{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} RgbColor;
+
+typedef struct HsvColor
+{
+    unsigned char h;
+    unsigned char s;
+    unsigned char v;
+} HsvColor;
+
+RgbColor HsvToRgb(unsigned char h, unsigned char s, unsigned char v)
+{
+    RgbColor rgb;
+    unsigned char region, remainder, p, q, t;
+
+    if (s == 0)
+    {
+        rgb.r = v;
+        rgb.g = v;
+        rgb.b = v;
+        return rgb;
+    }
+
+    region = h / 43;
+    remainder = (h - (region * 43)) * 6; 
+
+    p = (v * (255 - s)) >> 8;
+    q = (v * (255 - ((s * remainder) >> 8))) >> 8;
+    t = (v * (255 - ((s * (255 - remainder)) >> 8))) >> 8;
+
+    switch (region)
+    {
+        case 0:
+            rgb.r = v; rgb.g = t; rgb.b = p;
+            break;
+        case 1:
+            rgb.r = q; rgb.g = v; rgb.b = p;
+            break;
+        case 2:
+            rgb.r = p; rgb.g = v; rgb.b = t;
+            break;
+        case 3:
+            rgb.r = p; rgb.g = q; rgb.b = v;
+            break;
+        case 4:
+            rgb.r = t; rgb.g = p; rgb.b = v;
+            break;
+        default:
+            rgb.r = v; rgb.g = p; rgb.b = q;
+            break;
+    }
+
+    return rgb;
 }
+
+HsvColor RgbToHsv(RgbColor rgb)
+{
+    HsvColor hsv;
+    unsigned char rgbMin, rgbMax;
+
+    rgbMin = rgb.r < rgb.g ? (rgb.r < rgb.b ? rgb.r : rgb.b) : (rgb.g < rgb.b ? rgb.g : rgb.b);
+    rgbMax = rgb.r > rgb.g ? (rgb.r > rgb.b ? rgb.r : rgb.b) : (rgb.g > rgb.b ? rgb.g : rgb.b);
+
+    hsv.v = rgbMax;
+    if (hsv.v == 0)
+    {
+        hsv.h = 0;
+        hsv.s = 0;
+        return hsv;
+    }
+
+    hsv.s = 255 * long(rgbMax - rgbMin) / hsv.v;
+    if (hsv.s == 0)
+    {
+        hsv.h = 0;
+        return hsv;
+    }
+
+    if (rgbMax == rgb.r)
+        hsv.h = 0 + 43 * (rgb.g - rgb.b) / (rgbMax - rgbMin);
+    else if (rgbMax == rgb.g)
+        hsv.h = 85 + 43 * (rgb.b - rgb.r) / (rgbMax - rgbMin);
+    else
+        hsv.h = 171 + 43 * (rgb.r - rgb.g) / (rgbMax - rgbMin);
+
+    return hsv;
+}
+
 
 
 //Callback to get measurement data directly from nimbus
 void imageCallback(void* unused0, void* img, void* unused1) {
-    std::thread t[num_threads];
-    //Launch a group of threads
-    for (int i = 0; i < num_threads; ++i) {
-        t[i] = std::thread(call_from_thread, i);
-    }
-
     auto start = std::chrono::steady_clock::now();
 
     ImgHeader_t* header = nimbus_seq_get_header(img);
@@ -89,8 +159,6 @@ void imageCallback(void* unused0, void* img, void* unused1) {
     int16_t* z = nimbus_seq_get_z(img);
     uint8_t* conf = nimbus_seq_get_confidence(img);
 
-    test = nimbus_seq_get_x(img);
-    
     //Move valid points into the point cloud and the corresponding images
     for(int i = 0; i < (m_img_width*m_img_height); i++)
         {
@@ -102,16 +170,26 @@ void imageCallback(void* unused0, void* img, void* unused1) {
                 m_nimbus_cloud->points[i].y         = result[1];
                 m_nimbus_cloud->points[i].z         = result[2];
                 m_nimbus_cloud->points[i].intensity = ampl[i];
-                //m_range_image.data[i]               = std::min(std::max(z[i]/200, 0), 255);
-                //m_intensity_image.data[i]           = std::min(std::max(50+ampl[i]/40, 0), 255);
+                RgbColor rgb_range = HsvToRgb(std::min(std::max(z[i]/200, 0), 255),255,255);
+                m_range_image.data[i*3]             = rgb_range.r;
+                m_range_image.data[i*3+1]           = rgb_range.g;
+                m_range_image.data[i*3+2]           = rgb_range.b;
+                RgbColor rgb_intens = HsvToRgb(std::min(std::max(ampl[i]/10, 0), 255),255,255);
+                m_intensity_image.data[i*3]         = rgb_intens.r;
+                m_intensity_image.data[i*3+1]       = rgb_intens.g;
+                m_intensity_image.data[i*3+2]       = rgb_intens.b;
             }
             else{
                 m_nimbus_cloud->points[i].x         = NAN;
                 m_nimbus_cloud->points[i].y         = NAN;
                 m_nimbus_cloud->points[i].z         = NAN;
                 m_nimbus_cloud->points[i].intensity = NAN;
-                //m_range_image.data[i]               = 0;
-                //m_intensity_image.data[i]           = std::min(std::max(50+ampl[i]/40, 0), 255);
+                m_range_image.data[i*3]             = 0;
+                m_range_image.data[i*3+1]           = 0;
+                m_range_image.data[i*3+2]           = 0;
+                m_intensity_image.data[i*3]         = 0;
+                m_intensity_image.data[i*3+1]       = 0;
+                m_intensity_image.data[i*3+2]       = 0;
             }
         }
     auto end = std::chrono::steady_clock::now();
@@ -139,13 +217,12 @@ int main(int argc, char** argv)
     m_nimbus_cloud->header.frame_id = "nimbus";
     m_range_image.width = m_img_width;
     m_range_image.height = m_img_height;
-    m_range_image.encoding = "mono8";
-    m_range_image.data.resize(m_img_width*m_img_height);
+    m_range_image.encoding = "rgb8";
+    m_range_image.data.resize(m_img_width*m_img_height*3);
     m_intensity_image.width = m_img_width;
     m_intensity_image.height = m_img_height;
-    m_intensity_image.encoding = "mono8";
-    m_intensity_image.data.resize(m_img_width*m_img_height);
-
+    m_intensity_image.encoding = "rgb8";
+    m_intensity_image.data.resize(m_img_width*m_img_height*3);
 
 
     ROS_INFO_STREAM("Nimbus-userland version: " << nimbus_get_version());
