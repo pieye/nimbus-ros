5c047bc1c9637f134fd53f1c9dded4720d55ad60
Bjarne Johannsen <42101377+bjajoh@users.noreply.github.com>
2020-06-19 20:39:56 +0200

    Updated ROS melodic install script to buster

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
-sudo apt-get install python-pip python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential
-sudo apt-get install python-setuptools python-empy python-defusedxml python-netifaces
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
 sudo rosdep init
 rosdep update
-mkdir ~/ros_install_ws/
-cd ~/ros_install_ws/
+mkdir -p ~/ros_catkin_ws
+cd ~/ros_catkin_ws
 rosinstall_generator perception --rosdistro melodic --deps --wet-only --tar > melodic-perception-wet.rosinstall
 wstool init src melodic-perception-wet.rosinstall
-rosdep install --from-paths src --ignore-src --rosdistro melodic -y
-./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
-source ~/ros_install_ws/install_isolated/setup.bash
-echo 'source ~/ros_install_ws/install_isolated/setup.bash' >> ~/.bashrc
+cd ~/ros_catkin_ws
+rosdep install -y --from-paths src --ignore-src --rosdistro melodic -r --os=debian:buster
+sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic
+source /opt/ros/melodic/setup.bash
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
