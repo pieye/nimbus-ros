57b9076753cba9f552317021d0e1b0db57ca7b4b
Bjarne Johannsen <42101377+bjajoh@users.noreply.github.com>
2020-08-16 21:24:18 +0200

    Update to ROS noetic

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu buster main" > /etc/apt/sources.list.d/ros-noetic.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
+sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
 sudo rosdep init
 rosdep update
 mkdir -p ~/ros_catkin_ws
 cd ~/ros_catkin_ws
-rosinstall_generator perception --rosdistro melodic --deps --wet-only --tar > melodic-perception-wet.rosinstall
-wstool init src melodic-perception-wet.rosinstall
-rosdep install -y --from-paths src --ignore-src --rosdistro melodic -r --os=debian:buster
-sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic
-source /opt/ros/melodic/setup.bash
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+rosinstall_generator perception --rosdistro noetic --deps --wet-only --tar > noetic-perception-wet.rosinstall
+wstool init src noetic-perception-wet.rosinstall
+rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r --os=debian:buster
+sudo src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j1 -DPYTHON_EXECUTABLE=/usr/bin/python3
+source /opt/ros/noetic/setup.bash
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
